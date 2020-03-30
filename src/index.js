297191d248f63dd324b7304e25b41eefb6ef4815
Anton Groshev <a.groshev90@gmail.com>
2020-03-30 21:52:10 +0300

    add components

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import {HeaderComponent} from './components/header.component'
+import {NavigationComponent} from './components/navigationcomponent'
+
+new HeaderComponent('header')
+
+const navigation = new NavigationComponent('navigation')
